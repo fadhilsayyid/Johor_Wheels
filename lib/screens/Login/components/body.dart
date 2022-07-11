32ad16edc11c59691c4a043778163a7618fd68c9
fadhilsayyid <fadhilsayyid01@gmail.com>
2022-07-11 11:13:36 +0700

    login screen fix

--- lib/screens/Login/components/body.dart
+++ lib/screens/Login/components/body.dart
@@ -1,14 +1,20 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:johor_wheels/auth_services.dart';
-import 'package:johor_wheels/screens/Home/home_screen.dart';
+import 'package:johor_wheels/constants.dart';
+// import 'package:johor_wheels/auth_services.dart';
+// import 'package:johor_wheels/screens/Home/home_screen.dart';
 import 'package:johor_wheels/screens/Login/components/background.dart';
+import 'package:johor_wheels/screens/Login/components/or_divider.dart';
+import 'package:johor_wheels/screens/Login/components/social_icon.dart';
 import 'package:johor_wheels/screens/Signup/signup_screen.dart';
 import 'package:johor_wheels/components/already_have_an_account_acheck.dart';
 import 'package:johor_wheels/components/rounded_button.dart';
 import 'package:johor_wheels/components/rounded_input_field.dart';
 import 'package:johor_wheels/components/rounded_password_field.dart';
 import 'package:provider/provider.dart';
+import 'package:johor_wheels/services/auth_services.dart';
+
+import '../../Password/forgotPassword_screen.dart';
 
 // ignore: must_be_immutable
 class Body extends StatelessWidget {
@@ -21,14 +27,14 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    User firebaseUser = Provider.of<User>(context);
+    // User firebaseUser = Provider.of<User>(context);
     Size size = MediaQuery.of(context).size;
     return Background(
       child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: size.height * 0.09),
+            SizedBox(height: size.height * 0.05),
             RoundedInputField(
               hintText: "Your Email",
               onChanged: (value) {},
@@ -41,11 +47,38 @@ class Body extends StatelessWidget {
             RoundedButton(
                 text: "Log in",
                 press: () {
-                  AuthServices.signIn(
-                      emailController.text, passwordController.text);
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => HomeScreen(firebaseUser)));
+                  final String email = emailController.text.trim();
+                  final String password = passwordController.text.trim();
+
+                  if (email.isEmpty) {
+                    print("Email is Empty");
+                  } else {
+                    if (password.isEmpty) {
+                      print("Password is Empty");
+                    } else {
+                      context.read<AuthService>().signIn(
+                            email,
+                            password,
+                          );
+                    }
+                  }
+                  // AuthServices.signIn(
+                  //     emailController.text, passwordController.text);
+                  // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  //     builder: (context) => HomeScreen(firebaseUser)));
                 }),
+            SizedBox(height: size.height * 0.02),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => PasswordScreen()));
+              },
+              child: Text(
+                'Forgot password?',
+                style: TextStyle(
+                    color: kPrimaryColor, fontWeight: FontWeight.bold),
+              ),
+            ),
             SizedBox(height: size.height * 0.02),
             AlreadyHaveAnAccountCheck(
               press: () {
@@ -59,6 +92,24 @@ class Body extends StatelessWidget {
                 );
               },
             ),
+            OrDivider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SocalIcon(
+                  iconSrc: "assets/icons/facebook.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/twitter.svg",
+                  press: () {},
+                ),
+                SocalIcon(
+                  iconSrc: "assets/icons/google-plus.svg",
+                  press: () {},
+                ),
+              ],
+            )
           ],
         ),
       ),
