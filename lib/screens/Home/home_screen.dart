d588bf67a4cd488b888344acedecf7a7bf93b90e
fadhilsayyid <fadhilsayyid01@gmail.com>
2022-07-11 11:22:07 +0700

    change into stateful widget

--- lib/screens/Home/home_screen.dart
+++ lib/screens/Home/home_screen.dart
@@ -1,15 +1,23 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// import 'package:johor_wheels/screens/Home/components/body.dart';
 import 'package:johor_wheels/screens/Home/components/map_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  final User user;
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+// final User user;
 
-  HomeScreen(this.user);
+// HomeScreen(this.user);
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: MapScreen()); // Scaffold
+    return Scaffold(body: MapScreen());
   }
 }
