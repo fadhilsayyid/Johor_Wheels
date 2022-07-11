87220b0fcc661a536688d65857ac4a61a9d0e648
fadhilsayyid <fadhilsayyid01@gmail.com>
2022-07-11 11:14:35 +0700

    wrapper

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:johor_wheels/screens/Home/home_screen.dart';
+// import 'package:johor_wheels/screens/Home/home_screen.dart';
 import 'package:johor_wheels/screens/Welcome/welcome_screen.dart';
+import 'package:johor_wheels/screens/SplashScreen/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    User firebaseUser = Provider.of<User>(context);
-    return (firebaseUser == null) ? WelcomeScreen() : HomeScreen(firebaseUser);
+    final user = context.watch<User>();
+
+    if (user != null) {
+      return SplashScreen();
+    }
+    return WelcomeScreen();
+    // User firebaseUser = Provider.of<User>(context);
+    // return (firebaseUser == null) ? WelcomeScreen() : HomeScreen(firebaseUser);
   }
 }
