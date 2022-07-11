86b2bdaf4b53a238d8adb65f4f4660752a637818
fadhilsayyid <fadhilsayyid01@gmail.com>
2022-07-11 11:19:16 +0700

    Tour guide screen

---
+++ lib/screens/TourGuide/tourGuide_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:johor_wheels/screens/TourGuide/components/body.dart';
+
+class TourGuideScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TourGuide(),
+    );
+  }
+}
