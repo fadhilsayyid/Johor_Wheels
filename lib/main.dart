5956ea47cbcf0f3263b1d749a0e01e242eb0dd16
fadhilsayyid <fadhilsayyid01@gmail.com>
2022-07-11 11:11:29 +0700

    changeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:johor_wheels/auth_services.dart';
-// import 'package:johor_wheels/screens/Welcome/welcome_screen.dart';
 import 'package:johor_wheels/wrapper.dart';
 import 'package:provider/provider.dart';
+import 'package:johor_wheels/services/auth_services.dart';
+
+import 'DataHandler/appData.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,13 +16,23 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider.value(
-      value: AuthServices.firebaseUserStream,
-      initialData: null,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Home",
-        home: Wrapper(),
+    return ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: MultiProvider(
+        providers: [
+          Provider<AuthService>(
+            create: (_) => AuthService(FirebaseAuth.instance),
+          ),
+          StreamProvider(
+            initialData: null,
+            create: (context) => context.read<AuthService>().authStateChanges,
+          ),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Home",
+          home: Wrapper(),
+        ),
       ),
     );
   }
