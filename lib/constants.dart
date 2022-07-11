47d68fc56ff731edc7c8faf8f0430f4bf985e6d0
fadhilsayyid <fadhilsayyid01@gmail.com>
2022-07-11 11:08:12 +0700

    Const Colour

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const kPrimaryColor = Color(0xFF2B8DAA);
 const kSecondColor = Color(0xFF7499AC);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+Color blackColor = const Color(0xff000000);
+Color greyColor = const Color(0xffAAAAAA);
+Color whitegreyColor = const Color(0xffE5E5E5);
+Color yellowColor = const Color(0xffEDBE16);
+Color whiteColor = const Color(0xffFFFFFF);
+TextStyle poppinsTextStyle = GoogleFonts.poppins();
